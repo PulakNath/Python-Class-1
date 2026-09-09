658a2c27708ecb2b08b9d3ea1858d299db2962e7
PulakNath <hellopran2000@gmail.com>
2026-09-08 20:09:41 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+print("qwertyuiop")
+print(123)
+print("I am 11.", 11)
+print("Hello \n")
+print("Welcome to", end= ">>>")
+x = 39
+y = "Red"
+print(x)
+print(y)
+codingal = "codingal"
+print(codingal)
+age = input("Enter your age: ")
+print("Your age is:", age)
+import keyword
+print(keyword.kwlist)
